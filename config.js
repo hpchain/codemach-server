e86a35351b7fdc8f7f041dddaf3760c520849603
martin159 <martinlee159@163.com>
2018-11-13 17:15:40 +0800

    Modified config.js

--- config.js
+++ config.js
@@ -4,14 +4,16 @@ module.exports = {
   port: '3000',
   protocol: 'http://',
   host: {
+    // the host of production environment
     production: 'seed1.bumo.io:16002',
+    // the host of sandbox environment
     development: 'seed1.bumotest.io:26002',
+    // the host of sandbox environment
     sandbox: '127.0.0.1:36002'
   },
-  // Nginx proxy domain
+  // Nginx proxy domain or host
   domain: '127.0.0.1:3002',
 
-  // privatekey for sandbox env
-  // privateKey: 'privbzqBymaoQFspMZd92TeEzRSeeEJBSojXZeUP3RtXAKUbWBLGb1SQ',
+  // privatekey for sandbox environment
   privateKey: 'privC17CRRAjNvELrfbTFbLJstyGxk3unpPkRZCZoXRz13WEpN5SiHn8'
 }
